7474f819590804970aa6f3af1e7bb31d2de822b7
Leon Cox <leon.coxsw@gmail.com>
2023-08-05 16:14:56 +0400

    orbitConrols upgraded

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -10,10 +10,10 @@ export const CanvasComponent = ({ currentModel }) => {
   return (
     <div className={"canvas"}>
       <Suspense fallback={<div className={"fallback"}>loading...</div>}>
-        <Canvas onCreated={() => console.log("Canvas rendered")} camera={{ fov: 75, position: [3, 1.5, 1.5]}}>
+        <Canvas onCreated={() => console.log("Canvas rendered")} camera={{ fov: 75, position: [2.5, 1.5, 1.25]}}>
           <directionalLight />
           <pointLight position={[10, 10, 10]} />
-          <OrbitControls />
+          <OrbitControls target={[0, 0.2, 0]} maxPolarAngle={Math.PI * 0.50} maxDistance={10} enableDamping={true}/>
           <Model name={"/terrain/terrain.glb"} />
           <Model key={currentModel} name={currentModel} />
           <Environment path="/hdr/" files="lilienstein_2k.hdr" background />
