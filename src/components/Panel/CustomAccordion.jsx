9dfcf2551b710c257393b467c2960cc54c7fb049
Leon Cox <leon.coxsw@gmail.com>
2023-08-19 18:08:30 +0400

    filter was improved

--- src/components/Panel/CustomAccordion.jsx
+++ src/components/Panel/CustomAccordion.jsx
@@ -8,25 +8,13 @@ import { Accordion } from 'react-bootstrap';
 export const CustomAccordion = ({ onChangeModel, onAddModelToList, material, size, type }) => {
 
     const FILTERED_MODELS = {}
-    // console.log('settings:', material, size, type)
+    console.log('settings: ', material, size, type)
     Object.keys(MODELS).forEach(model => {
         FILTERED_MODELS[model] = [];
         MODELS[model].forEach(elem => {
-            // const materialRegExp = new RegExp(`${'\[|mu\]'}`);
-            // const sizeRegExp = new RegExp(`${'universal'}`);
-            // const typeRegExp = new RegExp(`${'universal'}`);
-            // const check = elem.name.match(materialRegExp)
-            // console.log(`${regexp} is in ${elem.name}`)
-            // if (check !== null){
-            //     console.log(`${materialRegExp} is in ${elem.name}: ${check}`)
-            // }
-            const pattern = `[${material}_${size}_${type}]`
-            if (elem.name.includes('[universal]')) {
-                // console.log('[universal]', elem.name)
-                FILTERED_MODELS[model].push(elem);
-            }
-            if (elem.name.includes(pattern)) {
-                // console.log(pattern, elem.name)
+            if ((elem.name.includes('[u_') || elem.name.includes(`[${material}_`))
+                && (elem.name.includes('_u_') || elem.name.includes(`_${size}_`))
+                && (elem.name.includes('_u]') || elem.name.includes(`_${type}]`))) {
                 FILTERED_MODELS[model].push(elem);
             }
         });
@@ -39,7 +27,7 @@ export const CustomAccordion = ({ onChangeModel, onAddModelToList, material, siz
 
     return (
 
-        <Accordion defaultActiveKey={null} style={{padding: "0px 6px 6px 6px"}}>
+        <Accordion defaultActiveKey={null} style={{ padding: "0px 6px 6px 6px" }}>
             {Object.keys(FILTERED_MODELS).map((model, index) => (
                 <Accordion.Item eventKey={index} key={index}>
                     <PrimaryButton name={model} eventKey={index}>
